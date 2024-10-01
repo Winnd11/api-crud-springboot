c5c3652a03745bf9dd770e43e783626101f8efd0
Winnd11 <recuperacaowilliamrios@gmail.com>
2024-10-01 10:17:22 -0300

    Add update path

--- src/main/java/org/william/cruddpt/controllers/UserController.java
+++ src/main/java/org/william/cruddpt/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.william.cruddpt.entities.User;
 import org.william.cruddpt.repositories.UserRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/db")
@@ -32,5 +33,20 @@ public class UserController {
     @PostMapping(value = "/insert")
     public User insertUser(@RequestBody User user) {
         return repository.save(user);
+    }
+
+    @PostMapping(value = "/update/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        Optional<User> result = repository.findById(id);
+        if (result.isPresent()) {
+            User userUpdate = result.get();
+            userUpdate.setName(user.getName());
+            userUpdate.setEmail(user.getEmail());
+            userUpdate.setDepartment(user.getDepartment());
+
+            return repository.save(userUpdate);
+        } else {
+            return null;
+        }
     }
 }
--- src/main/java/org/william/cruddpt/exceptions/NoSuchElementException.java
+++
@@ -1,5 +0,0 @@
-package org.william.cruddpt.exceptions;
-
-public class NoSuchElementException extends java.util.NoSuchElementException {
-
-}
